96d5826f21e8e70aaad86a56058000182151ea51
misrori <ormraat.pte@gmail.com>
2022-12-25 20:29:08 +0100

    day01

--- day01.py
+++ day01.py
@@ -1,1 +1,15 @@
-print('hello')
+t = open('day_01.txt', 'r')
+data = t.readlines()
+one_string = ''.join(data)
+pieces = one_string.split('\n\n')
+
+sums = []
+for i in pieces:
+    t = i.split('\n')
+    t = list(map(int, t))
+    sums.append(sum(t))
+print(max(sums))
+
+# part 2
+sums.sort(reverse=True)
+sum(sums[0:3])
